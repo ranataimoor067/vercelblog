a86c851969618e3ad0049c1ad2c24d2be58251ba
Taimoor <ranatamoor067@gmail.com>
2025-02-10 20:25:11 +0500

    Updated .gitignore to ignore temp/ directory

--- api/index.js
+++ api/index.js
@@ -44,7 +44,7 @@ app.get('*', (req, res) => {
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
+  res.status(statusCode).json({ 
     success: false,
     statusCode,
     message,
