c2aedcf44266f610bae4c4c4282522465b924c91
Taimoor <ranatamoor067@gmail.com>
2025-02-15 22:54:44 +0500

    Your descriptive commit message

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL || 'http://localhost:3000',
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Explicitly set the output directory
-    emptyOutDir: true, // Clear the output directory before building
-  },
+    plugins: [react()],
+    build: {
+      outDir: 'dist', // Output directory
+      emptyOutDir: true, // Clear the output directory before building
+    },
+  };
 });
